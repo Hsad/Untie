40106516b1167f98ad07fe0078c39b3b7ffa3851
hizzy <leeh14@rpi.edu>
2014-10-31 19:12:37 -0400

    working notes

---
+++ ProjectFiles/Assets/Scripts/Notebook_script.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Notebook_script : MonoBehaviour {
+	public string name;
+	// Use this for initialization
+	public Animator anim;
+	
+	void Awake(){
+		anim = GetComponent<Animator>();
+	}
+	
+	public void Update(){
+		anim.SetBool("moving",CameraLook.isInteracting);
+	}
+}
---
+++ ProjectFiles/Assets/Scripts/notes_changing.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class notes_changing : MonoBehaviour {
+	public Text notes;
+	// Use this for initialization
+	void Start () {
+		notes = GetComponent<Text>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		notes.text = ("hi i'm gosu  dsjfhlksjahsfkjasfhlaskjdhlsjdkfsahlfkjsdlas");
+	}
+}
