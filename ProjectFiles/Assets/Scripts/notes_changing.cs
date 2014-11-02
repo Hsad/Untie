f245339629271c4c9e6332abbce137779a0b3082
hizzy <leeh14@rpi.edu>
2014-11-02 01:04:08 -0400

    working clicking on prev and next pages

--- ProjectFiles/Assets/Scripts/notes_changing.cs
+++ ProjectFiles/Assets/Scripts/notes_changing.cs
@@ -1,17 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.UI;
 
 public class notes_changing : MonoBehaviour {
 	public Text notes;
+	public int num_pages =0;
+	private List<string> data = new List<string>();
 	// Use this for initialization
 	void Start () {
 		notes = GetComponent<Text>();
-
+		data.Add("hiyo");
+		data.Add("boo");
+		data.Add("halloween");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		notes.text = ("hi i'm gosu  dsjfhlksjahsfkjasfhlaskjdhlsjdkfsahlfkjsdlas");
+		//notes.text = ("his");
+		//check if need to add notes
+		//
+	}
+	public void page_change(){
+		num_pages ++;
+		notes.text = data[num_pages];
+	}
+	public void prev_page(){
+		if( num_pages > 0){
+			num_pages ++;
+			notes.text = data[num_pages];
+		}
 	}
 }
